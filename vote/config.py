2bc6c4ef0c3f46db74f118028f12cbfd8064df10
francescopennisi00 <pennisi.f@studium.unict.it>
2023-11-22 15:12:00 +0100

    warnings removed

--- vote/config.py
+++ vote/config.py
@@ -5,10 +5,12 @@ class BaseConfig:
     FLASK_APP = 'vote'
     FLASK_ENV = 'development'
     MYSQL_DATABASE_HOST = 'mysql'  # tanto per fare un esempio
-    MYSQL_DATABASE_PORT = os.getenv('MYSQL_DATABASE_PORT', 3306) # tanto per fare un esempio
+    MYSQL_DATABASE_PORT = os.getenv('MYSQL_DATABASE_PORT', 3306)  # tanto per fare un esempio
+
 
 class ProdConfig(BaseConfig):
     pass
+
 
 class DevConfig(BaseConfig):
     pass
